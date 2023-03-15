cb714a0b112a4eb71ed1adf1dfa7566e723a3bd4
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-03-15 07:11:50 -0700

    creating a route folder and feching products from the database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+const router = express.Router();
+
+
+// fetching all the products GET/api/products
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// getting a single product by id
+router.get("/:id",  asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+
+  if (product) {
+    res.json(product);
+  }else{
+      res.status(404).json({message: "product not found"})
+  }
+}));
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express"
-import products from "./data/products.js";
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 const app = express();
 
 dotenv.config()
@@ -15,16 +15,7 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.send("API IS RUNNING")
 })
-app.get("/api/products", (req, res) => {
-    res.json(products)
-})
-
-// getting a single product by id
-app.get("/api/products/:id", (req, res) => {
-    const{id} = req.params
-    const product = products.find((p) => p._id === id)
-    res.json(product)
-})
+app.use("/api/products", productRoutes)
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
